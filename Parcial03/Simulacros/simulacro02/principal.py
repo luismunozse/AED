54670da3029dd5fb8dc2f6016f553aa913ca7cd5
luismunozse <luismunozse@gmail.com>
2024-09-12 00:57:52 -0300

    simulacro 2

--- Parcial03/Simulacros/simulacro02/principal.py
+++ Parcial03/Simulacros/simulacro02/principal.py
@@ -7,11 +7,26 @@ def validar(n):
     while n <= 0:
         n = int(input('Incorrecto. Ingrese un numero valido: '))
 
+def cargar_tickets():
+    n = validar(0)
+    tickets = [None] * n
+    nombres = ('LT', 'AA', 'FB', 'JS', 'GOL')
+    for i in range(n):
+        cod = random.choice(nombres) + '00' + str(i)
+
+
 def principal():
     tickets = []
     opcion = -1
-
     while opcion != 5:
+        print('Menu de opciones')
+        print('*****************')
+        print('1 - Cargar tickets')
+        print('2 - Mostrar tickets')
+        print('3 - Calcular importe acumulado por pais')
+        print('4 - Buscar ticket por id de pasajero')
+        print('5 - Salir ')
+        opcion = int(input('Seleccione una opcion: '))
         if opcion == 1:
             pass
         if opcion == 2:
