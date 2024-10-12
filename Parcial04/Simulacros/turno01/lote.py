cc72e7f249d28a2f826b7980b7ecc7ce9712fb99
Luis Muñoz <luismunozse@gmail.com>
2024-10-12 11:54:14 -0300

    update tp04 y simulacros p04

---
+++ Parcial04/Simulacros/turno01/lote.py
@@ -0,0 +1,8 @@
+class Lote:
+    def __init__(self, prop, mza, lote, ubi, sup, monto):
+        self.propietario = prop
+        self.manzana = mza
+        self.lote = lote
+        self.ubicacion = ubi
+        self.superficie = sup
+        self.monto = monto
--- Trabajos Practicos/TP04/TP04/main.py
+++ Trabajos Practicos/TP04/TP04/main.py
@@ -8,12 +8,53 @@ def generar_archivo_binario(origen, destino):
         print(f'El archivo {origen} no existe')
         return
 
+    if os.path.exists(destino):
+        respuesta = input('El archivo ya existe. Deseas eliminarlo y crear uno nuevo? (s/n): ')
+        if respuesta.lower() == 's':
+            print('Eliminando archivo y creando uno nuevo ... ')
+            os.remove(destino)
+        else:
+            return
+
     datos = open(origen, 'rt')
 
+    salida = open(destino, 'wb')
+
+    for linea in datos:
+        d = linea.split(',')
+        cp = d[0]
+        dire = d[1]
+        tipo = int(d[2])
+        pago = int(d[3])
+        env = envio.Envio(cp, dire, tipo, pago)
+        pickle.dump(env, salida)
+
+    datos.close()
+    salida.close()
+
+
+def mostrar_archivo_binario(destino):
+    if not os.path.exists(destino):
+        print(f'EL archivo {destino} no existe')
+        return
+    print('Listado general de envios.....')
+    c = 0
+    m = open(destino, 'rb')
+    t = os.path.getsize(destino)
+    while m.tell() < t:
+        env = pickle.load(m)
+        print(env)
+        c += 1
+    m.close()
+    print('Se listaron ', c, ' registros...')
 
 
 def principal():
-    pass
+    origen = 'envios-tp4.csv'
+    destino = 'envios-tp4.dat'
+    generar_archivo_binario(origen, destino)
+    print('Terminado...')
+    mostrar_archivo_binario(destino)
 
 
 if __name__ == '__main__':
