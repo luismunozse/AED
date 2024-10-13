038e58d9e31ad5871067be9e0b97b7d9d36c9680
Luis Muñoz <luismunozse@gmail.com>
2024-10-13 10:36:59 -0300

    update tp04

--- Trabajos Practicos/TP04/TP04/main.py
+++ Trabajos Practicos/TP04/TP04/main.py
@@ -1,60 +1,45 @@
-import envio
+from envio import *
 import os.path
 import pickle
 
 
-def generar_archivo_binario(origen, destino):
-    if not os.path.exists(origen):
-        print(f'El archivo {origen} no existe')
+def crear_archivo_binario(ft, fb):
+    if os.path.exists(ft):
+        print('El archivo ', ft, 'no existe')
+        print('Controle y vuelva por aqui')
         return
 
-    if os.path.exists(destino):
-        respuesta = input('El archivo ya existe. Deseas eliminarlo y crear uno nuevo? (s/n): ')
-        if respuesta.lower() == 's':
-            print('Eliminando archivo y creando uno nuevo ... ')
-            os.remove(destino)
-        else:
-            return
+    datos = open(ft, 'rt')
+    ts = datos.readline()
+    hd = datos.readline()
 
-    datos = open(origen, 'rt')
-
-    salida = open(destino, 'wb')
-
+    m = open(fb, 'wb')
     for linea in datos:
         d = linea.split(',')
         cp = d[0]
-        dire = d[1]
-        tipo = int(d[2])
-        pago = int(d[3])
-        env = envio.Envio(cp, dire, tipo, pago)
-        pickle.dump(env, salida)
+        df = d[1]
+        te = d[2]
+        fp = d[3]
+        env = Envio(cp, df, te, fp)
+        pickle.dump(env, m)
 
     datos.close()
-    salida.close()
-
-
-def mostrar_archivo_binario(destino):
-    if not os.path.exists(destino):
-        print(f'EL archivo {destino} no existe')
-        return
-    print('Listado general de envios.....')
-    c = 0
-    m = open(destino, 'rb')
-    t = os.path.getsize(destino)
-    while m.tell() < t:
-        env = pickle.load(m)
-        print(env)
-        c += 1
     m.close()
-    print('Se listaron ', c, ' registros...')
+
+
+def mostrar_archivo_binario(fb):
+    if not os.path.exists(fb):
+        print('El archivo', fb, 'no existe')
+        print('Controle y vuelva por aqui')
+        return
+
+    print('Listado General de envios...')
+
+
 
 
 def principal():
-    origen = 'envios-tp4.csv'
-    destino = 'envios-tp4.dat'
-    generar_archivo_binario(origen, destino)
-    print('Terminado...')
-    mostrar_archivo_binario(destino)
+    pass
 
 
 if __name__ == '__main__':
